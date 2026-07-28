62f61bcf2e1480af3ea62cc45c9fad2ee4bd460f
John-Zevgolis <zevgolisgiannis@yahoo.gr>
2026-07-28 23:33:26 +0300

    delete employee error

--- api/src/main/java/com/assettracker/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/assettracker/api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.assettracker.api.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -34,5 +35,12 @@ public class GlobalExceptionHandler {
         
         errors.put("errors", fieldErrors);
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", "Cannot delete employee with assigned assets. Please unassign their assets first."));
     }
 }
