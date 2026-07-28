b6f1b12797b8a646c05033c7f82cad641a652b87
John-Zevgolis <zevgolisgiannis@yahoo.gr>
2026-07-28 22:52:31 +0300

    test

--- ui/src/api/axios.ts
+++ ui/src/api/axios.ts
@@ -5,7 +5,7 @@ import axios, {
 import router from '@/router';
 
 const api = axios.create({
-  baseURL: 'https://asset-tracker-api.up.railway.app/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
